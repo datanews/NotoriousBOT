baf6242e803b1a269190b5704b66e483f7722d82
Alan Palazzolo <apalazzolo@minnpost.com>
2015-05-28 16:31:22 -0500

    Adding super villians.

--- scripts/superheroes.js
+++ scripts/superheroes.js
@@ -8,14 +8,21 @@
 //
 // Commands:
 //   superhero - Gets a random super hero name
+//   supervillains - Gets a rancdom super villain name
 
 var superheroes = require('superheroes');
+var supervillains = require('supervillains');
 
 // Function for hubot
 module.exports = function(robot) {
 
   // superhero command
   robot.hear(/superhero|super hero|superheros|super heros/i, function(msg) {
-    msg.send(superheroes.random() + ' could be your superhero.');
+    msg.send('*' + superheroes.random() + '* could be your super hero.');
+  });
+
+  // supervillains command
+  robot.hear(/supervillain|super villain|supervillains|super villains|supervillian|supervillians/i, function(msg) {
+    msg.send('*' + supervillains.random() + '* could be your super villain.');
   });
 };
